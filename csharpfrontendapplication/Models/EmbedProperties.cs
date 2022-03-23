082733177e5bb857f7bd0118a811fc66774a3ec2
dhanasurya <dhanasurya.sf3454@gmail.com>
2022-03-23 11:39:37 +0530

    EmbededApplication_Front_End

---
+++ csharpfrontendapplication/Models/EmbedProperties.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Syncfusion.Server.EmbedApplication.Models
+{
+    public class EmbedProperties
+    {
+        //BoldBI server URL (ex: http://localhost:5000/bi, http://demo.boldbi.com/bi)
+        public static string RootUrl = "http://localhost:59102/bi/designer";
+
+        //For Bold BI Enterprise edition, it should be like `site/site1`. For Bold BI Cloud, it should be empty string.
+        public static string SiteIdentifier = "http://localhost:59102/bi/site/site1";
+
+        //Enter your BoldBI credentials here.
+        public static string UserEmail = "rajkumar.nachiappan@syncfusion.com";
+        
+        // Get the embedSecret key from Bold BI, please check this link(https://help.syncfusion.com/bold-bi/on-premise/site-settings/embed-settings)
+        public static string EmbedSecret = "3KeapmAM9suDtt57RCkIu7EywfKofitd";
+    }
+}
