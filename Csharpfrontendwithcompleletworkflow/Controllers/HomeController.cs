de416cf6afdcb555af66717d96768c804670b870
dhanasurya <dhanasurya.sf3454@gmail.com>
2022-03-25 15:05:42 +0530

    Final_front_end

---
+++ Csharpfrontendwithcompleletworkflow/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Syncfusion.Server.EmbedApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Syncfusion.Server.EmbedApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var nonce = Guid.NewGuid().ToString(); // random string
+            var userInfo = EmbedProperties.UserEmail; // email address of the user
+            bool canSaveView = true; // enable or disable permission to create, open, update, delete view 
+            bool hasViews = true; // enable or disable the permission to check the views of the dashboard
+            bool hasExport = true; // enable or disable the permission to export the dashboards and widgets
+            bool hasDashboardComments = true; // enable or disable the permission to comment related actions to dashboards
+            bool hasWidgetComments = true; // enable or disable the permission to comment related actions to widgets
+            bool isMarkFavorite = true; // enable to disable the permission to make the dashboard favorite
+            double timeStamp = DateTimeToUnixTimeStamp(DateTime.UtcNow); // current time as UNIX time stamp
+            var expirationTime = "100"; // alive time of the token
+
+            string embedMessage = "embed_nonce=" + nonce + "&embed_user_email=" + userInfo + "&embed_dashboard_views_edit=" + canSaveView + "&hide_header=" + false + "&embed_dashboard_views=" + hasViews + "&embed_dashboard_export=" + hasExport + "&embed_dashboard_comments=" + hasDashboardComments + "&embed_widget_comments=" + hasWidgetComments + "&embed_dashboard_favorite=" + isMarkFavorite + "&embed_timestamp=" + timeStamp + "&embed_expirationtime=" + expirationTime;
+
+            string signature = SignURL(embedMessage, EmbedProperties.EmbedSecret);
+            string embedSignature = embedMessage + "&embed_signature=" + signature;
+
+            ViewBag.EmbedSignature = embedSignature;
+
+            return View();
+        }
+
+        static string SignURL(string embedMessage, string secretcode)
+        {
+            var encoding = new UTF8Encoding();
+            var keyBytes = encoding.GetBytes(secretcode);
+            var messageBytes = encoding.GetBytes(embedMessage);
+            using (var hmacsha1 = new HMACSHA256(keyBytes))
+            {
+                var hashMessage = hmacsha1.ComputeHash(messageBytes);
+                return Convert.ToBase64String(hashMessage);
+            }
+        }
+
+        static double DateTimeToUnixTimeStamp(DateTime dateTime)
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            long unixTimeStampInTicks = (dateTime.ToUniversalTime() - unixStart).Ticks;
+            return unixTimeStampInTicks / TimeSpan.TicksPerSecond;
+        }
+    }
+}
---
+++ Csharpfrontendwithcompleletworkflow/Scripts/JavaScript.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    $('#but_upload').click(function () {
+        $("#upl").empty();
+        $("#upl").append('<img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"style="width: 50px; height: 20px; color:green" />', ' File is Uploading...');
+        var fd = new FormData();
+        var option = $("input[type='radio']:checked").val();
+        var files = $('#file')[0].files[0];
+        fd.append('file', files)
+        fd.append('options', option);
+        alert(fd);
+
+
+        $.ajax({
+
+            url: 'http://localhost:49054/api/file/savefile',
+            type: 'post',
+            data: fd,
+
+            contentType: false,
+            processData: false,
+            success: function (respose) {
+                alert(respose);
+                $("#upl").empty();
+                $("#upl").append('<img src="https://static9.depositphotos.com/1431107/1143/i/950/depositphotos_11437164-stock-photo-green-tick.jpg"alt=""style="width: 30px; height: 20px;" />', ' File uploaded Successfully');
+
+            },
+            error: function (respose) {
+                $("#upl").empty();
+                $("#upl").append('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWFntcWBEUs8GXR2NGvYddqKxoT-H5n4PBd_Qm_iaIPsrOEWIKaWYL7F3ft92M6ZTWd6Q&usqp=CAU" alt=""style="width: 25px; height: 20px;" />', ' File uploaded failed');
+
+            }
+
+
+
+        });
+
+
+
+
+    });
+});
+
+
+
